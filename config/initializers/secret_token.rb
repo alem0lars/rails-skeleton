6d1cc988bf40f305199746cdf0a8700bcc748570
Alessandro Molari <molari.alessandro@gmail.com>
2012-12-27 22:19:47 +0100

    repository import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlessandroNextreamLabsCom::Application.config.secret_token = '942b372d9004d137c94e06cb57ae70df58b330fdcb49717ee07ca5d02def5490a4e3580a2753151afc9bd8d8b609f6a0f1d5735720736f07782cc2488d6842f9'
